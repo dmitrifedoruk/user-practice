bdc988e896ca9c047c9fc5cad43b2f85ff1ae6d4
dmitrifedoruk <dmitrifedoruk@gmail.com>
2024-09-28 14:00:40 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+import './style.css'
+// import pg from 'pg';
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+<fieldset>
+<div><label>Username: </label><input id="username"></div>
+<div><label>Email: </label><input id="email"></div>
+<div><label>Password: </label><input id="password"></div>
+<button id="submitButton">Submit</button>
+
+</fieldset>
+
+<div id="output">
+
+</div>
+
+  </div>
+`
+
+
+
