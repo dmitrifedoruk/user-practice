0362018cf87a083ad06f8f69b820765f557b074d
dmitrifedoruk <dmitrifedoruk@gmail.com>
2024-10-02 00:51:30 -0400

    testing api

--- api/test.js
+++ api/test.js
@@ -1,7 +1,7 @@
 export async function POST(request) {
     const { body } = request;
 
-    let res = await new Response(`Hello ${body}, you just parsed the request body!`);
+    let res = await new Response(`Hello ${JSON.stringify(body)}, you just parsed the request body!`);
 
     return res;
 }
